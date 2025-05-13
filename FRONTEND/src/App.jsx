1fe30a870db1049cddd74023687b3fda7b685981
fareskebeh <fares.kebbeh@gmail.com>
2025-05-13 11:08:33 +0300

    Added a home component and route

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import Home from "./components/Home"
 
 const App = () => {
   return (
+    <Router>
     <div>
-      
+        <Routes>
+          <Route path='/' element={<Navigate to="/home"/>}/>
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
     </div>
+    </Router>
   )
 }
 
