e34739bf775cfe5f763fd0b66bc98a8cde533c08
fareskebeh <fares.kebbeh@gmail.com>
2025-05-13 11:15:39 +0300

    Added first route

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 import Home from "./components/Home"
-
+import Nav from './components/Nav'
 const App = () => {
   return (
     <Router>
-    <div>
+      <Nav/>
         <Routes>
           <Route path='/' element={<Navigate to="/home"/>}/>
           <Route path='/home' element={<Home/>}/>
         </Routes>
-    </div>
     </Router>
   )
 }
