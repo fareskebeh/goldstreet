c8e30399951f430528b1feab9748afaf773e9e18
fareskebeh <fares.kebbeh@gmail.com>
2025-05-13 12:00:51 +0300

    Styled the nav component

--- FRONTEND/src/components/Nav.jsx
+++ FRONTEND/src/components/Nav.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import GSIcon from "../assets/icons/GS.png"
 
 const Nav = () => {
+const location = useLocation()
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/pricing">Pricing</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+    <nav className="px-4 p-2 text-xl text-white flex items-center justify-between  bg-gray-900">
+        <div className="flex gap-2 items-center *:p-2 *:rounded-md *:transition-all *:duration-100 *:hover:bg-gray-800">
+            <img className="w-13" src={GSIcon} alt="" />
+            <Link className={`${location.pathname === "/home" && "text-[#ffdb8d] font-bold"}`} to="/home">Home</Link>
+            <Link className={`${location.pathname === "/pricing" && "text-[#ffdb8d] font-bold"}`} to="/pricing">Pricing</Link>
+            <Link className={`${location.pathname === "/about" && "text-[#ffdb8d] font-bold"}`} to="/about">About</Link>
+            <Link className={`${location.pathname === "/contact" && "text-[#ffdb8d] font-bold"}`} to="/contact">Contact</Link>
+        </div>
+        <Link className="rounded-md p-2 bg-gray-600" to="/register">Register</Link>
     </nav>
   );
 };
