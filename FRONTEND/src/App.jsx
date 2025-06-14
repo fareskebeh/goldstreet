67c4aedaed0862ad6dfb279696e0f9d83b32c4df
fareskebeh <fares.kebbeh@gmail.com>
2025-06-14 15:25:27 +0300

    General Structure

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import Home from './components/Home'
+import About from './components/About'
+import Login from './components/Login'
+import NotFound from './components/NotFound'
+import Pricing from './components/Pricing'
+import Register from './components/Register'
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Navigate to="/home"/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/pricing' element={<Pricing/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </Router>
   )
 }
 
