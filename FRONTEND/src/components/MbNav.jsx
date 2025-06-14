eb7b48b984b67674da4a24cba383bbfbe8e93dd8
fareskebeh <fares.kebbeh@gmail.com>
2025-06-14 18:13:32 +0300

    Minimal tweaks to styling

--- FRONTEND/src/components/MbNav.jsx
+++ FRONTEND/src/components/MbNav.jsx
@@ -9,8 +9,11 @@ const MbNav = ({ user }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="flex justify-between p-6 bg-gray-900">
-      <img className="w-10" src={GS} />
+    <div className="flex bg-gradient-to-b from-black to-transparent fixed top-0 left-0 right-0 justify-between p-6">
+      <Link to="/home" className="contents">
+        <img className="w-10" src={GS} />
+      </Link>
+
       <button onClick={() => setOpen(true)}>
         <HiMenu className="text-white" size={30} />
       </button>
@@ -22,7 +25,7 @@ const MbNav = ({ user }) => {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.4 }}
             onClick={() => setOpen(false)}
-            className={`fixed bg-[#0b111ed2] z-10 top-0 left-0 right-0 bottom-0`}
+            className={`fixed bg-black/80 z-10 top-0 left-0 right-0 bottom-0`}
           />
         )}
       </AnimatePresence>
@@ -30,60 +33,66 @@ const MbNav = ({ user }) => {
       <div
         className={`${
           open ? "" : "translate-x-100"
-        } transition duration-400 bg-gray-900 ${user? "" : "w-70"} z-20 flex flex-col justify-between p-6 fixed top-0 right-0 bottom-0`}
+        } transition duration-400 bg-neutral-950 ${
+          user ? "" : "w-70"
+        } z-20 flex flex-col justify-between p-6 fixed top-0 right-0 bottom-0`}
       >
         <div className="text-white font-bold text-2xl flex justify-between">
-            <p>Gold Street</p>
+          <p>Gold Street</p>
           <button onClick={() => setOpen(false)}>
             <HiX className="text-white" size={30} />
           </button>
         </div>
 
-        
-
         <div className="*:text-white *:rounded-lg *:transition duration-300 text-2xl flex flex-col gap-2 *:p-4">
           <Link
-            className={location.pathname === "/home" ? "bg-gray-700" : ""}
+            onClick={() => setOpen(false)}
+            className={location.pathname === "/home" ? "bg-neutral-900" : ""}
             to="home"
           >
             Home
           </Link>
           <Link
-            className={location.pathname === "/about" ? "bg-gray-700" : ""}
+            onClick={() => setOpen(false)}
+            className={location.pathname === "/about" ? "bg-neutral-900" : ""}
             to="about"
           >
             About
           </Link>
           <Link
-            className={location.pathname === "/pricing" ? "bg-gray-700" : ""}
+            onClick={() => setOpen(false)}
+            className={location.pathname === "/pricing" ? "bg-neutral-900" : ""}
             to="pricing"
           >
             Pricing
           </Link>
         </div>
 
-        {user ? 
-        (
-            <div className="flex items-center gap-4">
-                <div>
-                    <img className="w-15 rounded-full" src={user.pfp} alt="" />
-                </div>
-                <div>
-                    <p className="font-bold text-xl text-white">{user.username}</p>
-                    <p className="text-gray-400">See your account details</p>
-                </div>
-                <HiChevronRight className="text-white" size={35}/>
+        {user ? (
+          <div className="flex items-center gap-4">
+            <div>
+              <img className="w-15 rounded-full" src={user.pfp} alt="" />
             </div>
-            
-            
-            ) : (
-            
-            <div className="flex flex-col items-start gap-2">
-                <p className="text-gray-400">Already a member?</p>
-                <button className="text-2xl p-2 px-4 font-bold text-white bg-gray-500 rounded-lg">Log In</button>        
+            <div>
+              <p className="font-bold text-xl text-white">{user.username}</p>
+              <p className="text-neutral-400">See your account details</p>
             </div>
+            <HiChevronRight className="text-white" size={35} />
+          </div>
+        ) : (
+          <div className="flex flex-col items-start gap-2">
+            <p className="text-neutral-400">Already a member?</p>
+            <Link
+              to="/login"
+              onClick={() => setOpen(false)}
+              className="contents"
+            >
+              <button className="text-2xl p-2 px-4 font-bold  bg-white rounded-lg">
+                Log In
+              </button>
+            </Link>
+          </div>
         )}
-
       </div>
     </div>
   );
