21f48a5e5c376d14e29d3edf31e883b0aab8ef1b
fareskebeh <fares.kebbeh@gmail.com>
2025-06-21 19:11:32 -0700

    Community page

--- FRONTEND/src/components/Community.jsx
+++ FRONTEND/src/components/Community.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import BlurText from "../bits/BlurText";
+import ShinyText from "../bits/ShinyText";
 
 const Community = () => {
   return (
-    <div className=' text-white'>
-      Community
-    </div>
-  )
-}
+    <div className="p-2 sm:p-20 text-white">
+      <BlurText text="Our Telegram Community"
+        delay={150}
+        animateBy="words"
+        direction="top"
+        className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl sm:mb-8 font-bold text-white mb-2"/>
+      <ShinyText text="We have a Telegram community in which we post trading insights and signals, we offer multiple payment plans for subscription" disabled={false} speed={3} className='text-xl sm:text-2xl md:text-3xl lg:text-4xl w-[50%] sm:w-auto' />
 
-export default Community
+    </div>
+  );
+};
+
+export default Community;
