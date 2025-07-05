ad1704ccf5b5e23fc0e0419ab4c64383130d25fa
fareskebeh <fares.kebbeh@gmail.com>
2025-07-05 12:58:13 -0700

    Font size edit

--- FRONTEND/src/components/Home.jsx
+++ FRONTEND/src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className='p-4 flex flex-col gap-4'>
       <SplitText text="Ready to start your journey in the trading realm?"
-      className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl leading-relaxed font-bold text-left"
+      className="text-5xl sm:text-5xl md:text-5xl lg:text-7xl leading-relaxed font-bold text-left"
       delay={100}
       duration={0.6}
       ease="power3.out"
@@ -19,7 +19,7 @@ const Home = () => {
       to={{ opacity: 1, y: 0 }}
       threshold={0.1}
       rootMargin="-100px"/>
-      <ShinyText text="Gold Street has you covered" disabled={false} speed={3} className='text-xl sm:text-4xl' />
+      <ShinyText text="Gold Street has you covered" disabled={false} speed={3} className='text-2xl sm:text-4xl' />
     </div>
   )
 }
