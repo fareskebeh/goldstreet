c03324f2747a50b257dbe8a8d53aa011e4072b07
fareskebeh <fares.kebbeh@gmail.com>
2025-07-05 15:01:28 -0700

    Changed the Pricing plan

--- FRONTEND/src/components/Pricing.jsx
+++ FRONTEND/src/components/Pricing.jsx
@@ -1,61 +1,69 @@
-import React from "react";
 import { motion } from "framer-motion";
 import ShinyText from "../bits/ShinyText";
+import { HiCheck } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 const Pricing = () => {
-  const plans = [
-    {
-      name: "Starter",
-      perks: ["Core trade signals", "Telegram channel access"],
-      price: "$9.99",
-    },
-    {
-      name: "Committed",
-      perks: ["In-depth analysis", "Private chat access"],
-      price: "$29.99",
-    },
-    {
-      name: "Legend",
-      perks: [
-        "All signals, real-time",
-        "Direct contact with team",
-        "Full market strategy",
-      ],
-      price: "$49.99",
-    },
-  ];
+  const plan = {
+    perks: [
+      "Daily accurate scalping signals",
+      "Trades breakdown",
+      "Livestream trading",
+    ],
+    price: 0.99,
+  };
 
   return (
-    <div className="flex text-white flex-col gap-2 p-2 sm:p-8">
-      <motion.p
-        initial={{ opacity: 1, y: -10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.3 }}
-        className="text-4xl sm:text-4xl md:text-5xl font-bold"
-      >
-        Pricing
-      </motion.p>
-      <ShinyText
-        text="Pick a plan that fits your growth mindset"
-        disabled={false}
-        speed={3}
-        className="text-lg sm:text-2xl md:text-3xl"
-      />
-      <div className="flex gap-2 *:flex-1 sm:flex-row flex-col justify-between *:bg-black *:border-b-3 *:border-neutral-700 *:hover:-translate-y-2 *:cursor-pointer *:transition duration-300 *:active:-translate-y-2 *:rounded-lg">
-        {plans.map((plan, index) => (
-          <motion.div initial={{y:-10}} animate={{y:0}} transition={{duration: 0.3}} className="flex p-2 sm:p-4 sm:flex-col gap-2 items-start justify-between" key={index}>
-            <div className="">
-              <p className="text-xl sm:text-3xl font-bold">{plan.name}</p>
-              {plan.perks.map((perk, index) => (
-                <p key={index} className="text-neutral-500 text-xs sm:text-xl"> <span className="text-green-600 mr-2">✔</span> {perk}</p>
-              ))}
-            </div>
-            <p className="p-2 bg-white sm:text-2xl rounded-lg flex justify-center text-black font-bold">
-              {plan.price}
-            </p>
-          </motion.div>
-        ))}
+    <div className="flex text-white flex-col items-center  gap-3 p-2 sm:p-8">
+      <div className="space-y-2">
+        <motion.p
+          initial={{ opacity: 1, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3 }}
+          className="text-5xl sm:text-4xl md:text-5xl font-bold"
+        >
+          Pricing
+        </motion.p>
+
+        <ShinyText
+          text="Our plan has been engineered to be affordable to anybody who wants to make money quickly"
+          disabled={false}
+          speed={3}
+          className="text-2xl sm:text-2xl md:text-3xl"
+        />
       </div>
+      <motion.div
+        initial={{ opacity: 0, scale: "95%" }}
+        animate={{ opacity: 1, scale: "100%" }}
+        transition={{ duration: 0.3 }}
+        className="bg-black border flex flex-col gap-2 p-6 border-amber-400 rounded-2xl"
+      >
+        <p className="text-3xl sm:text-4xl font-bold">
+          Gold Street Subscription
+        </p>
+        <div className="flex flex-col">
+          {plan.perks.map((perk, index) => (
+            <div className="flex items-center gap-2 text-xl sm:text-2xl">
+              <HiCheck size={24} fill="#00ff00" />
+              <p>{perk}</p>
+            </div>
+          ))}
+        </div>
+        <div className=" flex flex-col gap-2 items-start">
+          <p className="text-xl sm:text-2xl cursor-default p-2 bg-gradient-to-br from-amber-400/70 font-bold via-amber-400/90 to-amber-400/70 inline-block rounded-lg">
+            ${plan.price} per week
+          </p>
+          <div className="sm:text-2xl text-xl sm:flex items-center space-y-4 sm:space-y-0 gap-4 justify-start">
+            <p className="underline">currently free until July 21st</p>
+            <Link
+              to="/register"
+              className="p-2 hover:scale-102 active:scale-100 transition duration-100 bg-white text-black rounded-lg font-bold"
+            >
+              Claim your spot
+            </Link>
+          </div>
+        </div>
+      </motion.div>
     </div>
   );
 };
