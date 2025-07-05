6481ecd808f7a70db10471b295db6c8a93f38a84
fareskebeh <fares.kebbeh@gmail.com>
2025-07-05 16:33:31 -0700

    Edited the community page content

--- FRONTEND/src/components/Community.jsx
+++ FRONTEND/src/components/Community.jsx
@@ -10,12 +10,12 @@ const Community = () => {
       <motion.div initial={{opacity:0, x:-40}} animate={{opacity:1, x:0}} transition={{duration:2}} className=" absolute -z-10 inset-0 overflow-hidden">
         <img style={{    maskImage: 'linear-gradient(to right, transparent, black 40%)',}} className=" w-auto h-full object-contain float-right rounded-4xl border-3 border-neutral-700" src={CommunityImageMb} alt="" />
       </motion.div>
-      <BlurText text="Our Telegram Community"
+      <BlurText text="Our Telegram Channel"
         delay={150}
         animateBy="words"
         direction="top"
         className="text-4xl sm:text-3xl md:text-4xl lg:text-5xl sm:mb-8 font-bold text-white mb-2"/>
-      <ShinyText text="We have a Telegram community in which we post trading insights and signals, we offer multiple payment plans for subscription" disabled={false} speed={3} className='text-2xl sm:text-2xl md:text-3xl lg:text-4xl w-[50%] sm:w-auto' />
+      <ShinyText text="We have a Telegram channel in which we post trading insights and tips, daily scalping signals and trade breakdowns" disabled={false} speed={3} className='text-2xl sm:text-2xl md:text-3xl lg:text-4xl w-[50%] sm:w-auto' />
 
     </div>
   );
